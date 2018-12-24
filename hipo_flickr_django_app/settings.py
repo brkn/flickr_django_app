73dd31b1066927c1c18871d843ae6d9a5ca1174a
Berkan <berkanunal@gmail.com>
2018-12-24 23:06:11 +0300

    Rename app to FlickrApp

--- hipo_flickr_django_app/settings.py
+++ hipo_flickr_django_app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic', #added by heroku
     'django.contrib.staticfiles',
+    'FlickrApp',
 ]
 
 MIDDLEWARE = [
