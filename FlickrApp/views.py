c84977ee8d8db12c73a5ae81552edc6b82d0d8de
Berkan <berkanunal@gmail.com>
2018-12-25 16:05:32 +0300

    Add urls and views file with dummy data

--- FlickrApp/views.py
+++ FlickrApp/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+dummy = [
+        {
+            "id": "46438529361",
+            "owner": "26982917@N02",
+            "secret": "913c4e83d9",
+            "server": "4910",
+            "farm": 5,
+            "title": "Porsche, 997 GT3RS, Wan Chai, Hong Kong",
+            "ispublic": 1,
+            "isfriend": 0,
+            "isfamily": 0
+        },
+        {
+            "id": "45525793405",
+            "owner": "156824316@N05",
+            "secret": "3e71c2185c",
+            "server": "7874",
+            "farm": 8,
+            "title": "",
+            "ispublic": 1,
+            "isfriend": 0,
+            "isfamily": 0
+        }
+]
+
+def index(request):
+    context = {
+        'photos': dummy
+    }
+    return render(request, 'FlickrApp/index.html', context)
