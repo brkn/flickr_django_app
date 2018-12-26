1b5382af2f5b38a36151bd74a9ce719a37458788
Berkan <berkanunal@gmail.com>
2018-12-26 16:11:54 +0300

    Format doc

--- FlickrApp/views.py
+++ FlickrApp/views.py
@@ -7,16 +7,16 @@ from .models import RecentSearch
 
 def getPhotos(url=None, tags=['kittens', 'puppies'], tag_mode='all', page=None):
     if url:
-        pass
+        pass  # TODO: fix here
     else:
         baseurl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1"
         api_key = '1ddb7df62cbdc4e07f6ec75ca78e2960'
         url = baseurl + "&api_key=" + api_key
         url += "&per_page=" + "20"
-        url += "&sort=" + "interestingness-desc"
-        url += "&media" + "photos"
+        url += "&sort=" + "relevance"
+        url += "&media=" + "photos"
 
-        url += "&tag_mode" + tag_mode
+        url += "&tag_mode=" + tag_mode
         url += "&tags="
         for tag in tags:
             url += tag + ","
@@ -40,6 +40,8 @@ def index(request):
         pass
     else:
         context['photos'], url = getPhotos()
-    context['photos'] = [dict(photo, link=f"https://farm{photo['farm']}.staticflickr.com/{photo['server']}/{photo['id']}_{photo['secret']}_o.jpg") for photo in context['photos']]
-    context['keywords'] =list(RecentSearch.objects.all().order_by('-date_entry').values())[:20]
+    context['photos'] = [dict(
+        photo, link=f"https://farm{photo['farm']}.staticflickr.com/{photo['server']}/{photo['id']}_{photo['secret']}.jpg") for photo in context['photos']]
+    context['keywords'] = list(
+        RecentSearch.objects.all().order_by('-date_entry').values())[:20]
     return render(request, 'FlickrApp/index.html', context)
